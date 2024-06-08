171fa680254541b607b874f0b8e4f3c4254496c4
Sooparerk Sakyai <sooparerk@mycostech.com>
2024-06-08 21:27:13 +0700

    Update Product entity

--- ShopXPress.Api/Entities/Product.cs
+++ ShopXPress.Api/Entities/Product.cs
@@ -1,15 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShopXPress.Api.Entities;
 
 [Table("Product")]
 public class Product : BaseEntity
 {
+    [Key]
     public int ProductId { get; set; }
     public string Name { get; set; }
     public string? Description { get; set; }
     public int InStock { get; set; }
     public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
     public int ProductCategoryId { get; set; }
     [ForeignKey(nameof(ProductCategoryId))]
     public ProductCategory ProductCategory { get; set; }
