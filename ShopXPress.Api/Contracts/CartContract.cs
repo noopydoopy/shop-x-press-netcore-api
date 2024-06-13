977d4d9803c62c8d5c075f5014d8c0a8c5ff17c1
sooparerk <sooparerk@mycostech.com>
2024-06-13 23:37:30 +0700

    Ad product price to cart

--- ShopXPress.Api/Contracts/CartContract.cs
+++ ShopXPress.Api/Contracts/CartContract.cs
@@ -18,6 +18,7 @@ public class CartProductContract
     public int Quantity { get; set; }
     public string? ProductName { get; set; }
     public string? ProductDescription { get; set; }
+    public decimal ProductPrice { get; set; }
 }
 
 public class CartDetailContract
