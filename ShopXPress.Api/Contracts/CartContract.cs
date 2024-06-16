744141550c5e838b5e332f2b133cc9fd9c418902
sooparerk <sooparerk@mycostech.com>
2024-06-16 15:11:08 +0700

    Add ImageUrl to product cart

--- ShopXPress.Api/Contracts/CartContract.cs
+++ ShopXPress.Api/Contracts/CartContract.cs
@@ -19,6 +19,7 @@ public class CartProductContract
     public string? ProductName { get; set; }
     public string? ProductDescription { get; set; }
     public decimal ProductPrice { get; set; }
+    public string? ProductImageUrl { get; set; }
 }
 
 public class CartDetailContract
